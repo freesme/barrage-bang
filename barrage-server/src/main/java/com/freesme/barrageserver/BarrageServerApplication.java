6caefc86ab154910565940a97727bf0ef5e752b2
lm <freesme@163.com>
2021-12-16 18:41:49 +0800

    第一次提交

---
+++ barrage-server/src/main/java/com/freesme/barrageserver/BarrageServerApplication.java
@@ -0,0 +1,13 @@
+package com.freesme.barrageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarrageServerApplication.class, args);
+    }
+
+}
---
+++ barrage-server/src/test/java/com/freesme/barrageserver/BarrageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freesme.barrageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
